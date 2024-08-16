d61afa0f983f8dd21317d7aed2d2790604fd212d
Emirhan Barlas <133690166+EmirhanBarlas@users.noreply.github.com>
2024-08-16 09:52:34 +0300

    Proje dosyası ekle.

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let random = Random()
+
+let GenerateRandomNumbers count min max =
+    List.init count (fun _ -> random.Next(min, max + 1))
+let randomNumbers = GenerateRandomNumbers 10 1 100
+
+printfn "Rastgele Sayılar: %A" randomNumbers
+
+let sum = List.sum randomNumbers
+printfn "Toplam: %d" sum
+
+let average = float sum / float (List.length randomNumbers)
+printfn "Ortalama: %f" average
+
+
+let threshold = 50
+let filteredNumbers = List.filter (fun x -> x > threshold) randomNumbers
+printfn "Eşik Değerinden Büyük Sayılar (%d): %A" threshold filteredNumbers
